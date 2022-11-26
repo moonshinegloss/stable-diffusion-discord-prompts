d2c3235f79e5b281e0803e7e77916d344ff9416f
moonshinegloss <118488816+moonshinegloss@users.noreply.github.com>
2022-11-26 15:19:18 +0100

    ammend version update

--- discord-prompt.user.js
+++ discord-prompt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         png metadata discord
 // @author       moonshine
-// @version      2.2
+// @version      2.3
 // @updateURL    https://raw.githubusercontent.com/moonshinegloss/stable-diffusion-discord-prompts/main/discord-prompt.user.js
 // @match        https://discord.com/channels/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
